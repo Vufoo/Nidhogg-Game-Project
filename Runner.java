576e0ddbba3b097bf016618d6776143b4e16398a
Vufoo <61858661+Vufoo@users.noreply.github.com>
2020-03-06 21:54:32 -0600

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -4,99 +4,160 @@ import java.awt.*;
 
 public class Runner {
 
-    private final int WIDTH = 100;
-    private final int HEIGHT = 100;
+
+    private boolean selected;
+
 
 
     public static void main(String[] args) {
+        ;
         new Runner();
 
     }
 
+
     public Runner(){
-        StdDraw.clear(StdDraw.WHITE);
-        StdDraw.setXscale(0, WIDTH);
-        StdDraw.setYscale(0, HEIGHT);
-        splashScreen();
+
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 100);
+        StdDraw.setYscale(0, 100);
+        for (int r = 0; true; r++) {
+
+            for (int i = 0; true; i++) {
+                splashScreen();
+                StdDraw.show();
+
+            }
+        }
     }
 
-    public void splashScreen(){
-        StdDraw.clear();
 
-        boolean choice = true;
+    public static void splashScreen(){
+
         int option = 0;
-        while(choice){
+
+        for (int r = 0; true; r++) {
+            StdDraw.setPenColor(Color.RED);
             StdDraw.text(50,60, "1. Single Player");
+            StdDraw.setPenColor(Color.BLUE);
             StdDraw.text(50,50, "2. Two Player");
+            StdDraw.setPenColor(Color.GREEN);
             StdDraw.text(50,40, "3. Controls");
-
-            if(StdDraw.isKeyPressed(KeyEvent.VK_1)){
+            StdDraw.show();
+            if (StdDraw.isKeyPressed(KeyEvent.VK_1)) {
                 option = 1;
-                choice = false;
             }
-            if(StdDraw.isKeyPressed(KeyEvent.VK_2)){
+            if (StdDraw.isKeyPressed(KeyEvent.VK_2)) {
                 option = 2;
-                choice = false;
             }
-            if(StdDraw.isKeyPressed(KeyEvent.VK_3)){
+            if (StdDraw.isKeyPressed(KeyEvent.VK_3)) {
                 option = 3;
-                choice = false;
+            }
+            if (option == 1) {
+                StdDraw.clear();
+                singlePlayer();
+            }
+            if (option == 2) {
+                StdDraw.clear();
+                twoPlayer();
+            }
+            if (option == 3) {
+                StdDraw.clear();
+                controls();
             }
 
         }
+    }
 
-        if(option == 1){
-            singlePlayer();
+
+    public static void singlePlayer(){ //single
+
+        Player player1 = new Player(40,10, 1,1, Color.BLUE,1);
+
+
+        while(true){
+            StdDraw.picture(50,50,"pixil-frame-0.png");//firstscreen
+            player1.draw();
+
+            if (StdDraw.hasNextKeyTyped()) {//controls
+                if (StdDraw.isKeyPressed(KeyEvent.VK_A)) {
+
+                    player1.moveLeft();
+                }
+                if (StdDraw.isKeyPressed(KeyEvent.VK_D)) {
+
+                    player1.moveRight();
+                }
+                if(StdDraw.isKeyPressed(KeyEvent.VK_ESCAPE)){//escape button
+                    StdDraw.clear();
+                    splashScreen();
+                }
+            }
+
+        StdDraw.show();
         }
-        if(option == 2){
-            twoPlayer();
-        }
-        if(option == 3){
-            controls();
+    }
+
+
+    public static void twoPlayer(){
+
+        Player player1 = new Player(40,10, 1,5, Color.BLUE,1);
+        Player player2 = new Player(60,10,1,5, Color.RED,2);
+
+
+        while(true){
+            StdDraw.picture(50,50,"pixil-frame-0.png");//firstscreen
+            player1.draw();
+            player2.draw();
+
+            if (StdDraw.hasNextKeyTyped()) { //controls
+                if (StdDraw.isKeyPressed(KeyEvent.VK_A)) {
+
+                    player1.moveLeft();
+                }
+                if (StdDraw.isKeyPressed(KeyEvent.VK_D)) {
+
+                    player1.moveRight();
+                }
+                if (StdDraw.isKeyPressed(37)) {
+
+                    player2.moveLeft();
+                }
+                if (StdDraw.isKeyPressed(39)) {
+
+                    player2.moveRight();
+                }
+                if(StdDraw.isKeyPressed(KeyEvent.VK_ESCAPE)){//escape button
+                    StdDraw.clear();
+                    splashScreen();
+                }
+            }
+            StdDraw.show();
         }
 
 
     }
 
-    public void firstScreen(){
-        boolean selected = true;
-        while(selected){
-            StdDraw.picture(50,50,"pixil-frame-0.png");
 
-            if(StdDraw.isKeyPressed(KeyEvent.VK_ESCAPE)){
-                selected = false;
+    public static void controls(){
+
+        for (int r = 0; true; r++) {
+            StdDraw.textLeft(50,50, "Player");
+
+            if(StdDraw.isKeyPressed(KeyEvent.VK_ESCAPE)){//escape button
+                StdDraw.clear();
+                splashScreen();
             }
         }
-        splashScreen();
-
-
-
     }
 
-    public void nextScreen(){
+
+    public static void firstScreen(){
 
     }
-
-    public void singlePlayer(){
-        firstScreen();
-        Player player1 = new Player(40,0, 5,5, Color.BLUE);
-        player1.draw();
-        while(true){
-
-
-        }
-    }
-
-    public void twoPlayer(){
-        firstScreen();
-        Player player1 = new Player(40,0, 5,5, Color.BLUE);
-        Player player2 = new Player(60,0,5,5, Color.RED);
-        while(true){
-
-        }
-    }
-
-    public void controls(){
+    public static void nextScreen(){
 
     }
 }
+
+
