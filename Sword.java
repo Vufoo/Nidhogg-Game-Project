07f6eab359262c0aefcd2c391b9f3e07034c4799
Vufoo <61858661+Vufoo@users.noreply.github.com>
2020-04-01 12:19:15 -0500

    Update Sword.java

--- Sword.java
+++ Sword.java
@@ -1,6 +1,102 @@
-public class Sword extends Player{
+import java.awt.*;
 
-    public void moveUp(){ }
+public class Sword extends GameObject{
+    int position;
+    double playernum;
+    boolean direction;
 
-    public void moveDown(){}
+    public Sword(){
+        super();
+        position = 0;
+        playernum = 0;
+        direction = false;
+    }
+
+    public Sword(double x, double y, int position, double player, boolean direction) {
+        super(x,y);
+        this.position = position;
+        this.playernum = player;
+        this.direction = direction;
+    }
+
+    public void draw(){
+        if(playernum == 20){
+
+            if(position == 0){
+                StdDraw.picture(x+10, y-5, "sword1.png");
+                StdDraw.circle(x+15, y-5, 1);
+            }
+            if(position == 1){
+                StdDraw.picture(x+10, y, "sword1.png");
+                StdDraw.circle(x+15, y, 1);
+            }
+            if(position == 2){
+                StdDraw.picture(x+10, y+5, "sword1.png");
+                StdDraw.circle(x+15, y+5, 1);
+            }
+
+        }
+
+        if(playernum == 80) {
+            if(position == 0){
+                StdDraw.picture(x-10, y-5, "sword2.png");
+                StdDraw.circle(x-15, y-5, 1);
+            }
+            if(position == 1){
+                StdDraw.picture(x-10, y, "sword2.png");
+                StdDraw.circle(x-15, y, 1);
+            }
+            if(position == 2){
+                StdDraw.picture(x-10, y+5, "sword2.png");
+                StdDraw.circle(x-15, y+5, 1);
+            }
+
+        }
+
+    }
+
+    public void hitBox(){
+
+    }
+
+    public void moveUp(){ position += 1; y+=5; }
+
+    public void moveDown(){ position -= 1; y-=5;}
+
+    public void moveLeft(double velx){ x -= velx; }
+
+    public void moveRight(double velx){
+        x += velx;
+    }
+
+    public int getPosition(){
+        return position;
+    }
+
+    public void setPosition(int position){
+        this.position = position;
+    }
+
+    public double getY(){
+        return y;
+    }
+
+    public void setY(double y){
+        this.y = y;
+    }
+
+    public double getX(){ return x; }
+
+    public void setX(){ this.x = x; }
+
+    public boolean getDirection(){
+        return direction;
+    }
+
+    public void parry(Sword sword){ //sword collision
+
+    }
+    public void collision(){  //player collision
+
+    }
 }
